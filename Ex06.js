dfe35b8c3cfa8756d400fd3f780a864020ead461
Nguyễn Văn Tuấn Nam <nn1307634@gmail.com>
2025-03-07 15:08:55 +0700

    Update Ex06.js

--- Ex06.js
+++ Ex06.js
@@ -1,6 +1,6 @@
 let arr = ["", false, 0, 5, 10, NaN, undefined, null, "Hello"];
 for (let i = 0; i < arr.length; i++){
-    if (arr[i] === false || arr[i] === 0 || arr[i] === "" || isNaN(arr[i]) || arr[i] === undefined || arr[i] === null){
+    if (!arr[i]){
         arr.splice(i, 1);
         i--;
     }
